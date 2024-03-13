cda941bc7e03f43534446a0aba87fe56206bf00e
Rodyaaaa <160117492+Rodyaaaa@users.noreply.github.com>
2024-03-13 10:13:28 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,12 @@
 // Copyright 2021 NNTU-CS
 
-int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+int cbinsearch(int* arr, int size, int value) {
+    if (size == 1) {
+        if (arr[0] == value)
+        {
+            return 1;
+        }
+        return 0;
+    }
+    return cbinsearch(arr, size / 2, value) + cbinsearch(arr + size / 2,size - size / 2, value);
 }
