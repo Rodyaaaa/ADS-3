e92363f81db578974030dc0bdccc728caada465b
Rodyaaaa <160117492+Rodyaaaa@users.noreply.github.com>
2024-03-13 10:28:39 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,11 +2,11 @@
 
 int cbinsearch(int* arr, int size, int value) {
     if (size == 1) {
-        if (arr[0] == value)
-        {
+        if (arr[0] == value) {
             return 1;
         }
         return 0;
     }
-    return cbinsearch(arr, size / 2, value) + cbinsearch(arr + size / 2,size - size / 2, value);
+    return cbinsearch(arr, size / 2, value)
+    + cbinsearch(arr + size / 2, size - size / 2, value);
 }
